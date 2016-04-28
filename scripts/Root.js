4cb4be69de459dbeb752efb1b8f9afb296bbda44
zhimin <zhimin@meizu.com>
2016-04-28 20:32:43 +0800

    修订主页

--- scripts/Root.js
+++ scripts/Root.js
@@ -8,6 +8,9 @@ import Button from './pages/button/index';
 
 import {queryString} from './utils/SearchQuery';
 
+/**
+ * 根目录接收code，tableId（可选）参数
+ */
 export default class Root extends Component {
     static propTypes = {
         history: PropTypes.object.isRequired
@@ -19,32 +22,40 @@ export default class Root extends Component {
     componentWillMount() {
         queryString(location.search, (err, query) => {
             if (err) {
+                // TODO 处理错误信息
                 console.log(err);
             } else {
+                // TODO 缓存桌台编码
                 console.log(query);
+                if (query.code) {
+                    console.log(query.code);
+                } else {
+                    console.log("Missing code from wechat");
+                }
             }
         })
     }
 
-    requireWechat(nextState, replace) {
-        // queryString(location.search, (err, query) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(query);
-        //     }
-        // });
+    requireSignUp(nextState, replace) {
+        queryString(location.search, (err, query) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(query);
+            }
+        });
         console.log('onEnter call...');
-        replace({pathname: '/signup'});
+        // replace({pathname: '/signup'});
     }
 
     render() {
         const {history} = this.props;
         return (
             <Router history={history}>
-                <Route name='explore' path='/' component={App}>
-                    <IndexRoute onEnter={this.requireWechat} component={Button}/>
-                    <Route path="table" name="table" component={Table}/>
+                <Route onEnter={this.requireSignUp} name='explore' path='/' component={App}>
+                    <IndexRoute component={Button}/>
+                    // 桌台状态信息，只能匹配/table/id 这种形式
+                    <Route path="table/:id" name="table" component={Table}/>
                 </Route>
                 <Route path="/signup" name="button" component={SignUp}/>
             </Router>
